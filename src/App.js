87cc021e842639373165807ee060d572971070bc
Digonzalezc <danyg2406@gmail.com>
2023-02-02 14:33:13 -0500

    corriendo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Series from "./pages/Series";
+import Comics from "./pages/Comics";
+import Error404 from "./pages/Error404";
+import Header from "./componets/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="/inicio" element={<Home></Home>} />
+          <Route path="/series"  element={<Series></Series>}/>
+          <Route path="/comics" element={<Comics></Comics>} />
+          <Route element={<Error404 />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
